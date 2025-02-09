ba9ceec4712010fbfed6c5deab3be72d96406640
Maksim Kozyanin <makc2214@gmail.com>
2025-02-09 12:40:39 +0300

    fix whitespaces and newline handling

--- src/app/utility/FileIO.java
+++ src/app/utility/FileIO.java
@@ -1,12 +1,14 @@
 package app.utility;
+
 import java.io.*;
 
 public class FileIO {
 
     public static Serializable[] read(String filename) {
 
-        int objectCount = countObjectsInFile(filename); // Количество объектов в файле
+        replaceMultipleNewlines(filename);
 
+        int objectCount = countObjectsInFile(filename); // Количество объектов в файле
         Serializable[] array = new Serializable[objectCount]; // Массив сериализуемых объектов величиной равной количеству объектов в файле
         int index = 0; // индекс для записи объектов
 
@@ -14,6 +16,7 @@ public class FileIO {
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
             StringBuilder stringBuilder = new StringBuilder();
             String line;
+            String[] trimmedObjectArray = stringBuilder.toString().trim().split("\n");
             while ((line = reader.readLine()) != null) {
                 //Если строка не пустая, добавляем ее к буферу StringBuilder
                 if (!line.trim().isEmpty()) {
@@ -28,9 +31,13 @@ public class FileIO {
                     Десериализуем строку в объект.
                     Записываем объект в массив.
                      */
-                    array[index++] = ObjectSerializer.deserialize(stringBuilder.toString().trim().split("\n"));
+                    array[index++] = ObjectSerializer.deserialize(trimmedObjectArray);
                     stringBuilder.setLength(0); // Очищаем буфер строки под следующий объект
                 }
+            }
+            // Десериализовать последний объект если конец файла не пустая строка
+            if (!stringBuilder.isEmpty()) {
+                array[index] = ObjectSerializer.deserialize(trimmedObjectArray);
             }
         } catch (IOException e) {
             System.err.println("Ошибка чтения объектов из файла: " + e.getMessage());
@@ -40,18 +47,18 @@ public class FileIO {
         return array;
     }
 
-    //Считает количество объектов в файле по количеству пустых строк
+    //Считает количество объектов в файле
     private static int countObjectsInFile(String filename) {
+
+        int objectCount = 0;
+
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
             String line;
-            int objectCount = 0;
-            boolean prevLineEmpty = false;
 
             while ((line = reader.readLine()) != null) {
-                if (line.trim().isEmpty() && !prevLineEmpty) {
+                if (line.trim().equalsIgnoreCase("car") || line.trim().equalsIgnoreCase("book") || line.trim().equalsIgnoreCase("vegetable")) {
                     objectCount++;
                 }
-                prevLineEmpty = line.trim().isEmpty();
             }
 
             return objectCount;
@@ -66,8 +73,11 @@ public class FileIO {
     // Запись в файл
     public static void write(String filename, Serializable[] objects) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true))) {
+            writer.newLine();
+            writer.newLine();
+
             for (Serializable obj : objects) {
-                String strObj = obj.toString().replace(",", "\n");
+                String strObj = obj.toString().replace(",", "\n").replace("\n ", "\n");
                 writer.write(String.format("%s%n", strObj));
                 writer.newLine();
             }
@@ -75,4 +85,26 @@ public class FileIO {
             System.err.println("Ошибка записи объектов в файл: " + e.getMessage());
         }
     }
+
+    //Замена множественных пустых строк на одну
+    private static void replaceMultipleNewlines(String filename) {
+        // Чтение из файла
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            writer.write(sb.toString().trim().replaceAll("(\\r?\\n){2,}", "\n\n"));
+        } catch (
+                IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
