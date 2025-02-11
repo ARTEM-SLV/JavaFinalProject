0a0cc1a090c5901b003753824ac8c5a47d66645a
Egor <egorshacklein098@gmail.com>
2025-02-11 17:24:03 +0700

    Created Validator class

--- src/app/validation/Validator.java
+++ src/app/validation/Validator.java
@@ -1,4 +1,21 @@
 package app.validation;
 
-public class Validator {
+public final class Validator {
+
+    private Validator(){};
+
+    public static String validateString(String string)
+    {
+        return !string.matches("\\s*") ? string : null;
+    }
+
+    public static Double validateDouble(String string){
+        return (string.matches(("\\d+(\\.\\d+)?")) && !string.matches("\\s*"))
+                ? Double.parseDouble(string) : null;
+    }
+
+    public static Integer validateInteger(String string){
+        return (string.matches(("\\d+")) && !string.matches("\\s*"))
+                ? Integer.parseInt(string) : null;
+    }
 }
